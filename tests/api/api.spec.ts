5220401362e8f9508a7b0e3fce0aa362bbdd29cc
Varju Zoli <zoltn.varju@gmail.com>
2022-09-19 16:02:28 +0200

    POST requests defined

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, request } from '@playwright/test'
 
 test.describe.parallel('API Testing', () => {
   const baseUrl = 'https://reqres.in/api'
@@ -24,5 +24,39 @@ test.describe.parallel('API Testing', () => {
     expect(responseBody.data.email).toBeTruthy()
   })
 
-  //test('POST Request')
+  test('POST Request - Create New User', async ({ request }) => {
+    const newName = 'morpheus'
+    const response = await request.post(`${baseUrl}/users/`, {
+      data: {
+        name: newName,
+        job: 'leader',
+      },
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(201)
+    expect(responseBody.name).toBe(newName)
+  })
+
+  test('POST Request - Login', async ({ request }) => {
+    const response = await request.post(`${baseUrl}/login`, {
+      data: {
+        email: 'eve.holt@reqres.in',
+        password: 'cityslicka',
+      },
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(200)
+    expect(responseBody.token).toBeTruthy()
+  })
+
+  test('POST Request - Login failed', async ({ request }) => {
+    const response = await request.post(`${baseUrl}/login`, {
+      data: {
+        email: 'eve.holt@reqres.in',
+      },
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(400)
+    expect(responseBody.error).toBe('Missing password')
+  })
 })
